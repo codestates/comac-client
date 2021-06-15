449f27391a1e3b29bc18e5f609ac142568ee5e6d
z1Hoon <ddungbar2@gmail.com>
2021-06-15 14:23:37 +0900

    feat_to-mypage-btn

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from '../components/Nav'
 import Post from '../components/Post'
 import './Home.css'
 
 
 const Home = () => {
+  const [PostList, SetPostList] = useState(null)
+  
+  useEffect(()=>{
+    //post_id
+  })
+
     return (
       <div className="home__body">
 
