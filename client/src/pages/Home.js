a8bdd4bb9b9f8255347e40daeb7c5c7d43640c3c
z1Hoon <ddungbar2@gmail.com>
2021-06-15 20:01:13 +0900

    for merging

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,16 +6,24 @@ import './Home.css'
 
 const Home = () => {
   const [PostList, SetPostList] = useState(null)
-  
-  useEffect(()=>{
-    //post_id
-  })
+  //post리스트
+  //서버로 요청 
+
+  // useEffect(async()=>{
+    //const data = await axios.get('https://coMac/api/post')
+    //SetPostList(data)
+    //
+  // },[PostList])
 
     return (
       <div className="home__body">
 
         <Nav/>
         <div className="post-list">
+          {/* {PostList ? PostList.map((data,index) => {
+            return <Post post={post}/>
+          }) : <div className="nothing"> 게시물이 없습니다 </div>
+          } */ }
           <Post/>
           <Post/>
           <Post/>
