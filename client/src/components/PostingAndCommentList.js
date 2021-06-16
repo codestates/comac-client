2377cf53a1ff2c4b30e35640771992eb6a0d2837
lhb7021 <manevil6@gmail.com>
2021-06-16 18:47:54 +0900

    edit:components

--- client/src/components/PostingAndCommentList.js
+++ client/src/components/PostingAndCommentList.js
@@ -7,24 +7,26 @@ const PostingAndCommentList = ({CommentList, CloseModal, postId, accessToken}) =
   const [comment, setComment] = useState("")
   //댓글작성 state
 
+
+  const handleCommentClick = async () => {
+    //서버로 댓글작성 요청 후 모달 닫기
+    await server.post(`/comment/${postId}`,{content: comment},{headers: accessToken})  /*보낼데이터(onchange입력값)*/
+    CloseModal()
+    window.location.reload()
+    //댓글작성 -> 1.요청후 꺼짐 2.
+  }
+
     return (
       <div className="overlay-modal">
         <div className="PostingAndCommentList__body">
           <div className="PostingAndCommentList__comment-list">
 
-            {CommentList.data.data.map((commentData,index)=> <CommentBox key={index} commentData={commentData}/>)}
+            {CommentList.data.data && CommentList.data.data.map((commentData,index)=> <CommentBox key={index} commentData={commentData}/>)}
 
           </div>
           <div className="PostingAndCommentList__form-box">
             <textarea onChange={(e)=>setComment(e.target.value)}>{null}</textarea>
-            <div onClick={async()=>
-            { 
-              //서버로 댓글작성 요청 후 모달 닫기
-              server.post(`/comment/${postId}`,{content: comment},{header: accessToken})  /*보낼데이터(onchange입력값)*/
-              CloseModal()
-              //댓글작성 -> 1.요청후 꺼짐 2.
-            }
-            }>등록</div>
+            <div onClick={handleCommentClick} >등록</div>
           </div>
         </div>
       </div>
